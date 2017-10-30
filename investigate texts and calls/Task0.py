0ca1c20ad030b9e18e6acd386d804520f2e5bdcd
VV-Para <1032566866@qq.com>
2017-10-30 09:37:58 +0800

    Update Task0.py

--- investigate texts and calls/Task0.py
+++ investigate texts and calls/Task0.py
@@ -11,6 +11,14 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+ """
+任务0:
+短信记录的第一条记录是什么？通话记录最后一条记录是什么？
+输出信息:
+"First record of texts, <incoming number> texts <answering number> at time <time>"
+"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
+"""
+
 #读取第一条短信记录和最后一条通话记录：
 def task0(list_0,list_1):
     text_0 = 'First record of texts, {} texts {} at time {}'.format(list_0[0][0],list_0[0][1],list_0[0][2])
@@ -20,10 +28,3 @@ def task0(list_0,list_1):
     return(text_0,call_1)
 
 task0(texts,calls)
-"""
-任务0:
-短信记录的第一条记录是什么？通话记录最后一条记录是什么？
-输出信息:
-"First record of texts, <incoming number> texts <answering number> at time <time>"
-"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
-"""
