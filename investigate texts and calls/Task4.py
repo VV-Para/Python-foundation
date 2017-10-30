c03d1693e69385b7bb1b3c7bd6a85ac43a6e7fe8
VV-Para <1032566866@qq.com>
2017-10-30 09:37:11 +0800

    Update Task4.py

--- investigate texts and calls/Task4.py
+++ investigate texts and calls/Task4.py
@@ -12,6 +12,18 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+"""
+任务4:
+电话公司希望辨认出可能正在用于进行电话推销的电话号码。
+找出所有可能的电话推销员:
+这样的电话总是向其他人拨出电话，
+但从来不发短信、接收短信或是收到来电
+请输出如下内容
+"These numbers could be telemarketers: "
+<list of numbers>
+电话号码不能重复，每行打印一条，按字母顺序输出。
+"""
+
 #提取主叫/发送电话号码
 def send(list):
     list0 = []
@@ -87,14 +99,3 @@ def main(list1,list2):
 
 main(calls,texts)
 
-"""
-任务4:
-电话公司希望辨认出可能正在用于进行电话推销的电话号码。
-找出所有可能的电话推销员:
-这样的电话总是向其他人拨出电话，
-但从来不发短信、接收短信或是收到来电
-请输出如下内容
-"These numbers could be telemarketers: "
-<list of numbers>
-电话号码不能重复，每行打印一条，按字母顺序输出。
-"""
